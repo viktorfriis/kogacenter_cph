71162b235a7771b17c35ef81c76809c6030e015a
Viktor Kjeldal <viktorfriiskjeldal@gmail.com>
2019-11-05 12:46:47 +0100

    header fra js

---
+++ main.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    hentHeader();
+}
+
+async function hentHeader() {
+    console.log("header");
+
+    const theHeader = await fetch("header.html");
+    let theFetched = await theHeader.text();
+    document.querySelector("header").innerHTML = theFetched;
+
+    console.log("header hentet");
+    burgerAktiv();
+}
+
+function burgerAktiv() {
+    console.log("Burger aktiv");
+
+    const burgerKnap = document.querySelector("#burger_knap");
+    const menu = document.querySelector("#menu");
+
+    burgerKnap.addEventListener("click", () => {
+        console.log("openMenu");
+
+        burgerKnap.classList.toggle("open"); //Selve knappen toggler klassen .open, hvilket animerer den til et kryds
+        burgerKnap.classList.toggle("pos_fixed"); //Knappen toggler position fixed, for at den bliver i toppen af skærmen når den er åben
+
+        menu.classList.toggle("toggle_menu"); //Selve menuen toggler klassen .toggle_menu, som åbner og lukker menuen
+    })
+}
