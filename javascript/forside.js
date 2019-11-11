9a7c037e618d344dac1769ccc7fc711832089df0
Viktor Kjeldal <viktorfriiskjeldal@gmail.com>
2019-11-11 22:40:48 +0100

    Om os indhold lavet dynamisk

--- javascript/forside.js
+++ javascript/forside.js
@@ -36,7 +36,8 @@
     //Funktionen nedenfor "shuffler", altså blander arrayet, så rækkefølgen af cyklerne er forskellig hver gang.
     //https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
     function shuffle(a) {
-        var j, x, i;
+        let j, x, i;
+
         for (i = a.length - 1; i > 0; i--) {
             j = Math.floor(Math.random() * (i + 1));
             x = a[i];
